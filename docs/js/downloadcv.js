2e8714f89c883c3238f58ee6c741a681ca123e3b
Wendy <202201363@cityofmalabonuniversity.edu.ph>
2025-12-18 15:59:53 +0800

    Delete docs/js/downloadcv.js

--- docs/js/downloadcv.js
+++
@@ -1,7 +0,0 @@
-
-    document.getElementById('downloadCV').addEventListener('click', function() {
-        const link = document.createElement('a');
-        link.href = './images/WendyPesimo_Internship_Resume.pdf'; 
-        link.download = 'WendyPesimo_Internship_Resume.pdf'; //file name
-        link.click(); // triggers the download
-    });
